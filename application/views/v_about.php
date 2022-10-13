2e0f9fc41b60141f1de6c6ba8505986d04ae8fbc
jahfalhumam <113886152+jahfalhumam@users.noreply.github.com>
2022-10-13 19:38:18 +0700

    Menambahkan v_about.php

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Jahfal</li>
+ <li>Nama Belakang : Ahmad</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li>Cibarusah Jaya</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Bekasi</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Jogging</li>
+ <li>Bulu tangkis</li>
+ </ul>
+</section>
